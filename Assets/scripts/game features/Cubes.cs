555c8fe998ac4517c6115a2cb34092132d183939
Gonçalo Afonso <goncaloafonso456@gmail.com>
2025-11-16 17:42:41 +0000

    the movement is good enough, I still need a life time for each cube

--- Assets/scripts/game features/Cubes.cs
+++ Assets/scripts/game features/Cubes.cs
@@ -1,19 +1,15 @@
 using UnityEngine;
 
-public class cubessystem : MonoBehaviour
+public class Cubes : MonoBehaviour
 {
-    public GameObject cubePrefab;
     private int cubeToSkip;   // Guarda o ID do cubo que NÃO vai ser instanciado
+    public GameObject cubePrefab;
+
 
     private void Start()
     {
-        game_started();
-    }
-    private void game_started()
-    {
         SpawnCubes();
     }
-
     private int cube_choice()
     {
         cubeToSkip = Random.Range(1, 7);   // De 1 a 6
@@ -22,19 +18,13 @@ public class cubessystem : MonoBehaviour
     }
     private void SpawnCubes()
     {
+        int cube_skipped = cube_choice();
         for (int i = 0; i < CubesPosClass.positions.Length; i++)
         {
-            if (i == cubeToSkip)
+            if (i == cube_skipped)
                 continue; // pula o cubo escolhido
 
             Instantiate(cubePrefab, CubesPosClass.positions[i], Quaternion.identity);
         }
-    }
-
-
-
-    private void cube_movement()
-    {
-        //para o movimento dos cubos
     }
 }
